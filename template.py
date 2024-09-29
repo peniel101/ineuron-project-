e6b77a106ce90cd52a98653eea170ccd3154dcaa
Fiawornu Etor Peniel <89279360+peniel101@users.noreply.github.com>
2024-09-29 15:09:55 +0000

    fixed structure files

--- template.py
+++ template.py
@@ -17,14 +17,14 @@ list_of_files = [
     "src/utils/__init__.py",
     "src/utils/utils.py", 
     "src/logger/logging.py", 
-    "src/exceptions/exception"
+    "src/exception/exception.py", 
     "test/unit/__init__.py", 
     "test/integration/__init__.py", 
     "init_setup.sh", 
     "requirements.txt", 
     "requirements_dev.txt", 
     "setup.py", 
-    "setup.config", 
+    "setup.cfg", 
     "pyproject.toml", 
     "tox.ini", 
     "experiment/experiments.ipynb"
